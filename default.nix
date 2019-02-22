c7421bd9dfe70ca8d732cbfd06d2e563000546c8
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-02-22 16:12:51 +0100

    new env-haskell

--- default.nix
+++ default.nix
@@ -1,2 +1,23 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, aeson, base, binary, cereal, deepseq, hashable
+, lens, log-domain, mtl, QuickCheck, semirings, stdenv, tasty
+, tasty-quickcheck, tasty-th, vector, vector-th-unbox
+}:
+mkDerivation {
+  pname = "SciBaseTypes";
+  version = "0.0.0.1";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson base binary cereal deepseq hashable lens log-domain mtl
+    semirings vector vector-th-unbox
+  ];
+  testHaskellDepends = [
+    base QuickCheck tasty tasty-quickcheck tasty-th
+  ];
+  benchmarkHaskellDepends = [
+    aeson base binary cereal deepseq hashable lens log-domain mtl
+    semirings vector vector-th-unbox
+  ];
+  homepage = "https://github.com/choener/SciBaseTypes";
+  description = "Base types and classes for statistics, sciences and humanities";
+  license = stdenv.lib.licenses.bsd3;
+}
