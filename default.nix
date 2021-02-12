39cde2ac3ee1cbb8a5733eca06707b2b29d96bf4
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2021-02-12 10:02:20 +0100

    updated default.nix

--- default.nix
+++ default.nix
@@ -1,23 +1,23 @@
-{ mkDerivation, aeson, base, binary, cereal, deepseq, hashable
-, lens, log-domain, mtl, QuickCheck, semirings, stdenv, tasty
-, tasty-quickcheck, tasty-th, vector, vector-th-unbox
+{ mkDerivation, aeson, base, binary, cereal, deepseq, DPutils
+, hashable, lens, log-domain, mtl, QuickCheck, semirings, stdenv
+, tasty, tasty-quickcheck, tasty-th, vector, vector-th-unbox
 }:
 mkDerivation {
   pname = "SciBaseTypes";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base binary cereal deepseq hashable lens log-domain mtl
-    semirings vector vector-th-unbox
+    aeson base binary cereal deepseq DPutils hashable lens log-domain
+    mtl semirings vector vector-th-unbox
   ];
   testHaskellDepends = [
-    aeson base binary cereal deepseq hashable lens log-domain mtl
-    QuickCheck semirings tasty tasty-quickcheck tasty-th vector
+    aeson base binary cereal deepseq DPutils hashable lens log-domain
+    mtl QuickCheck semirings tasty tasty-quickcheck tasty-th vector
     vector-th-unbox
   ];
   benchmarkHaskellDepends = [
-    aeson base binary cereal deepseq hashable lens log-domain mtl
-    semirings vector vector-th-unbox
+    aeson base binary cereal deepseq DPutils hashable lens log-domain
+    mtl semirings vector vector-th-unbox
   ];
   homepage = "https://github.com/choener/SciBaseTypes";
   description = "Base types and classes for statistics, sciences and humanities";
